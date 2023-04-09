ec07d27ddb1a3137d4d237be5f3a94e24f802d0b
ashishjauto <50689398+ashishjauto@users.noreply.github.com>
2023-04-09 14:54:17 +0530

    added project springboot
    added project

---
+++ src/main/java/com/aj/webservices/demo/webservide/DemoWebservideApplication.java
@@ -0,0 +1,13 @@
+package com.aj.webservices.demo.webservide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebservideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebservideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aj/webservices/demo/webservide/DemoWebservideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.webservices.demo.webservide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebservideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
